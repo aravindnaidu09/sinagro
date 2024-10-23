383c9828f84978067fef3e8edef54007d38aa3b1
aravindnaidu09 <aravindbabu372@gmail.com>
2024-10-23 21:51:37 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './public/**/*.html',   // Make sure this points to the location of your HTML files
+    './src/**/*.css',       // This points to your custom CSS with Tailwind imports
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#D1D4A2', // Light olive green
+        secondary: '#706B43', // Muted brown
+      },
+      fontFamily: {
+        sans: ['Maven Pro', 'sans-serif'], // Adding Maven Pro font
+      },
+    },
   },
   plugins: [],
-}
-
+};
